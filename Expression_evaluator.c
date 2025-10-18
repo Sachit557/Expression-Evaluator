11f99111af7d3c52b160436ff0b89c557d1358ea
Sachit557 <sachitgu11@gmail.com>
2025-10-19 00:26:26 +0530

    work in progress

--- Expression_evaluator.c
+++ Expression_evaluator.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
+
+char* Space_remover(char expression[50]);
+
+
 int main()
 {
-printf("testing 1 2 3\n");
-
-return 0;
+    Space_remover("Hello world");
+    return 0;
 }
+
+char* Space_remover(char expression[50])
+{
+    int count = 0;
+    char expression_copy[50];
+    for ( int i = 0 ; expression[i] != '\0' ;i++)
+    {   
+        count++;
+        if ( count > 40)
+        {
+            printf("cant be greater than 40\n");
+            break;
+        }
+    }  
+    
+    for ( int i = 0 ; i < count ; i++)
+    {
+        expression_copy[i] = expression[i];
+    }
+    
+    for ( int i = 0 ; i < count ; i++)
+    {
+        if ( expression[i] == ' ')
+        {
+            int space_counter = 0;
+            for ( int j = i; j < count; j++)
+            {
+                space_counter++;
+                if (count == j+1)
+                {
+                    break;
+                }
+                expression_copy[j] = expression[j+1];
+            }
+            expression_copy[count-1-space_counter] = '\0';
+
+        }
+
+
+    }
+    int counter = 0;
+    for ( int i = 0 ; expression_copy[i] != 0 ; i++)
+    {
+        counter++;
+        printf("%c" , expression_copy[i]);
+    }
+    printf("%d" , counter);
+    
+
+
+}   
