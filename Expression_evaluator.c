27a82c2dcdfe4b3614b61c4adf0cf1e1e5cc22f5
Sachit557 <sachitgu11@gmail.com>
2025-11-03 22:17:24 +0530

    yes

--- Expression_evaluator.c
+++ Expression_evaluator.c
@@ -1,107 +1,95 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "string.h"
 
-void expression_eval(char expression[40]);                                // it must take string input
+void expression_eval(char expression[40]); // it must take string input
 void bracket(char expression[40]);
-char* space_remove(char expression[40]);
-
-
-
+char *space_remove(char expression[40]);
 
 char clear_expression[40];
-int expression_bracket_size = 20;    // set at 20 can be increased
-
+int expression_bracket_size = 20; // set at 20 can be increased
 
 int main()
 {
-    char string[] = "(4+(5-1))+(7*2)";
+    char string[] = "(4+(5-1))+(7*2)"; // test string - Hardcoded
     space_remove(string);
     return 0;
 }
 
-char* space_remove(char expression[40])
+char *space_remove(char expression[40])
 {
-    
-    int i , j = 0;
-    for (i = 0 ; expression[i] != '\0' ; i++)
+
+    int i, j = 0;
+    for (i = 0; expression[i] != '\0'; i++)
     {
         if (expression[i] != ' ')
         {
             clear_expression[j] = expression[i];
-            j++;                    // the letters in the string counted
+            j++; // the letters in the string counted
         }
     }
-    clear_expression[j] = '\0';    // clear expression has the string with spaces removed
+    clear_expression[j] = '\0'; // clear expression has the string with spaces removed
     return clear_expression;
-
 }
 
 void bracket(char expression[40])
 {
-    int op = 0 , cb = 0;                                 // op for (  
-    for (int i = 0 ; i < strlen(expression);i++)         // cb for )
+    int op = 0, cb = 0;                          // open bracket (op) for '('
+    for (int i = 0; i < strlen(expression); i++) // close bracket (cb) for '('
     {
-        if(expression[i] == '(')
+        if (expression[i] == '(')
         {
             op++;
         }
 
-        if( expression[i] == ')')
+        if (expression[i] == ')')
         {
             cb++;
         }
-
     }
 
-    if ( op != cb)
+    if (op != cb)
     {
         printf("Number of open brackets are not equal to number of closed brackets\n");
         return;
     }
 
-    for (int i = 0 ; i < strlen(expression) ; i++)
+    for (int i = 0; i < strlen(expression); i++)
     {
         if (expression[i] == '(')
         {
-            for (int j = i ; j < strlen(expression) ; j++)
+            for (int j = i; j < strlen(expression); j++)
             {
                 if (expression[j] == ')' || expression[j] == '(')
                 {
-                    if ( expression[j] == '(')
+                    if (expression[j] == '(')
                     {
-                        break;   // same bracket again :D
+                        break; // same bracket again :D
                     }
 
                     if (expression[j] == ')')
                     {
-                        int n =0;
+                        int n = 0;
                         char expression_bracket[expression_bracket_size];
-                        for (int k =i+1 ; k < j;k++)
+                        for (int k = i + 1; k < j; k++)
                         {
-                            if ( n < expression_bracket_size -1)
+                            if (n < expression_bracket_size - 1)
                             {
-                                expression_bracket[n++] = expression[k];     // substring  
-                            }                
+                                expression_bracket[n++] = expression[k]; // substring
+                            }
                             // append values into the expression bracket from the stuff in bracket
                             // send to the expression eval with a string to eval it
                         }
-                        expression_bracket[n] ='\0';
-                            
+                        expression_bracket[n] = '\0';
                     }
 
                     else
                     {
                         break;
                     }
-
                 }
-
-                
-
-                
             }
         }
     }
 }
-
--- string.c
+++ string.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // string replace
 // string length
 // string append
-// substring finder 
+// substring finder
 
 int string_length(char string[50]);
-char string_append(char string[50] , char append[50]);
-char string_replace(int start , int end , char string[40] , char new_text[40]);
-char substring_finder(int start , int end , char string[40]);
-
+char *string_append(char string[50], char append[50]);
+char *string_replace(int start, int end, char string[40], char new_text[40]);
+char *substring_finder(int start, int end, char string[40]);
 
 int string_length(char string[50])
 {
@@ -18,71 +18,69 @@ int string_length(char string[50])
     return a;
 }
 
-char string_append(char string[50] , char append[50])
-{ 
+char *string_append(char string[50], char append[50])
+{
     int string_length = strlen(string);
     int append_string_length = strlen(append);
-    int total =  string_length + append_string_length + 1;
+    int total = string_length + append_string_length + 1;
     char appended_string[total];
 
-    for (int i = 0 ; i < string_length ; i++)
+    for (int i = 0; i < string_length; i++)
     {
         appended_string[i] = string[i];
     }
 
-    for ( int j = 0 ; j < append_string_length ; j++ )
+    for (int j = 0; j < append_string_length; j++)
     {
         appended_string[string_length + j] = append[j];
     }
 
-    appended_string[total-1] = '\0';
+    appended_string[total - 1] = '\0';
 
     // return the appended string
-
 }
 
-
-char string_replace(int start , int end , char string[40] , char new_text[40])
+char *string_replace(int start, int end, char string[40], char new_text[40])
 {
 
-    //replace the text between i and j to the new text where i and j are included
-    int difference = end - start -1;
+    // replace the text between i and j to the new text where i and j are included
+    int difference = end - start - 1;
     int new_str_length = string_length(new_text);
     char updated_string[40];
     int k = 0;
-    
-    for ( int i = 0 ; i < start && string[i] != '\0' ; i++)
+
+    for (int i = 0; i < start && string[i] != '\0'; i++)
     {
         updated_string[k] = string[i];
         k++;
     }
 
-    for (int i = 0; new_text[i] != '\0'; i++) {
+    for (int i = 0; new_text[i] != '\0'; i++)
+    {
         updated_string[k] = new_text[i];
         k++;
     }
 
-    for (int i = end + 1; string[i] != '\0'; i++) {
+    for (int i = end + 1; string[i] != '\0'; i++)
+    {
         updated_string[k] = string[i];
         k++;
     }
-    
-    updated_string[k] ='\0';
-    // return the updated string
 
+    updated_string[k] = '\0';
+    // return the updated string
 }
 
-char substring_finder(int starting , int ending , char string[40])
+char *substring_finder(int starting, int ending, char string[40])
 {
     char substring[40];
-    int difference = ending - starting +1;
-    for ( int i = starting ; i <= ending ; i++)
+    int difference = ending - starting + 1;
+    for (int i = starting; i <= ending; i++)
     {
-        string_append(substring ,string[i] );
+        string_append(substring, string[i]);
     }
 
     substring[difference] = '\0';
 
-    //return substring
-    
+    // return substring
 }
